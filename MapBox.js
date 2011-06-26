2a6b1a029e7fd3de6762af744a3a4cfb5a788dfe
Tom MacWright <macwright@gmail.com>
2011-06-26 18:18:48 -0400

    Using issues instead of TODO file. Normalizing style with fixjsstyle.

--- MapBox.js
+++ MapBox.js
@@ -3,64 +3,64 @@ var ss = SpreadsheetApp.getActiveSpreadsheet(),
     sheet = ss.getActiveSheet(),
     activeRange = ss.getActiveRange(),
     settings = {};
-    headersRaw = getHeaders(sheet, activeRange,1);
+    headersRaw = getHeaders(sheet, activeRange, 1);
 
 // Add menu for MapBox functions
 function onOpen() {
-  var menuEntries = [ {name: "Export GeoJSON", functionName: "toGeoJSON"},
-                      {name: "Help", functionName: "helpSite"} ];
-  ss.addMenu("MapBox", menuEntries);
+  var menuEntries = [{name: 'Export GeoJSON', functionName: 'toGeoJSON'},
+                      {name: 'Help', functionName: 'helpSite'}];
+  ss.addMenu('MapBox', menuEntries);
 }
 
 // Export selected range to GeoJSON
 function toGeoJSON() {
   // Create a new UI
   var app = UiApp.createApplication().setTitle('Export GeoJSON').setStyleAttribute('width', '460');
-  
+
   // Create a grid to hold the form
   var grid = app.createGrid(4, 2);
-  
+
   // Add form elements to the grid
   grid.setWidget(0, 0, app.createLabel('Unique ID:'));
-  grid.setWidget(0, 1, app.createListBox().setName('idBox').setId('idBox')); 
+  grid.setWidget(0, 1, app.createListBox().setName('idBox').setId('idBox'));
   grid.setWidget(1, 0, app.createLabel('Longitude:'));
-  grid.setWidget(1, 1, app.createListBox().setName('longBox').setId('longBox')); 
+  grid.setWidget(1, 1, app.createListBox().setName('longBox').setId('longBox'));
   grid.setWidget(2, 0, app.createLabel('Latitude:'));
-  grid.setWidget(2, 1, app.createListBox().setName('latBox').setId('latBox')); 
-  
+  grid.setWidget(2, 1, app.createListBox().setName('latBox').setId('latBox'));
+
   // Set the list boxes to the header values
   for (var i = 0; i < headersRaw.length; ++i) {
-    app.getElementById("idBox").addItem(headersRaw[i]);
-    app.getElementById("longBox").addItem(headersRaw[i]);
-    app.getElementById("latBox").addItem(headersRaw[i]);
+    app.getElementById('idBox').addItem(headersRaw[i]);
+    app.getElementById('longBox').addItem(headersRaw[i]);
+    app.getElementById('latBox').addItem(headersRaw[i]);
   }
-  
+
   // Create a vertical panel...
   var panel = app.createVerticalPanel().setId('settingsPanel');
 
   var description = app.createLabel(
     'To format your spreadsheet as GeoJSON file, select the following columns:'
-  ).setStyleAttribute('margin-bottom','20');
+  ).setStyleAttribute('margin-bottom', '20');
   panel.add(description);
-  
+
   // ...and add the grid to the panel
   panel.add(grid);
-  
-  // Create a button and click handler; pass in the grid object as a 
+
+  // Create a button and click handler; pass in the grid object as a
   // callback element and the handler as a click handler
   // Identify the function b as the server click handler
   var button = app.createButton('Export')
-                  .setStyleAttribute('margin-top','10')
+                  .setStyleAttribute('margin-top', '10')
                   .setId('export');
   var handler = app.createServerClickHandler('b');
   handler.addCallbackElement(grid);
   button.addClickHandler(handler);
-  
-  // Add the button to the panel and the panel to the application, 
+
+  // Add the button to the panel and the panel to the application,
   // then display the application app in the Spreadsheet doc
   grid.setWidget(3, 1, button);
   app.add(panel);
-  app.setStyleAttribute('padding','20');
+  app.setStyleAttribute('padding', '20');
   ss.show(app);
 }
 
@@ -72,32 +72,32 @@ function b(e) {
     'long': e.parameter.longBox,
     'lat': e.parameter.latBox
   };
-  
+
   // Get the UI object
   var app = UiApp.getActiveApplication();
-  
+
   // Update the settings button (this is not firing in before the call to exportGeoJSON())
   app.getElementById('export')
-    .setText("Exporting...")
+    .setText('Exporting...')
     .setEnabled(false);
-  
+
   // Hide the settings panel
   var settingsPanel = app.getElementById('settingsPanel');
   settingsPanel.setVisible(false);
-  
+
   // Create GeoJSON file and pass back it's filepath
   var filePath = exportGeoJSON();
-  
+
   // Notify the user that the file is done and in their Google Docs list
   var description = app.createLabel('The GeoJSON file has been saved in your Google Docs List.')
-                       .setStyleAttribute('margin-bottom','10');
+                       .setStyleAttribute('margin-bottom', '10');
   app.add(description);
-  
+
   // And provide a link to it
-  var fileLink = app.createAnchor('Download GeoJSON File',filePath)
-                    .setStyleAttribute('font-size','150%');
+  var fileLink = app.createAnchor('Download GeoJSON File', filePath)
+                    .setStyleAttribute('font-size', '150%');
   app.add(fileLink);
-  
+
   // Update the UI.
   return app;
 }
@@ -110,19 +110,19 @@ function getHeaders(sheet, range, columnHeadersRowIndex) {
 }
 
 function exportGeoJSON() {
-  
+
   // Set up the response object
   var response = {
-    "type": "FeatureCollection",
-    "features": []
+    'type': 'FeatureCollection',
+    'features': []
   };
 
   // For every row active range, make feature object
-  var features = getRowsData(sheet, activeRange,1);
+  var features = getRowsData(sheet, activeRange, 1);
   response.features = features;
 
   // Output GeoJSON
-  var outputFileName = normalizeHeader(ss.getName())+"-"+Date.now()+".geojson",
+  var outputFileName = normalizeHeader(ss.getName()) + '-'+ Date.now() + '.geojson',
       stringToDisplay = JSON.stringify(response, null, 2),
       file = DocsList.createFile(outputFileName, stringToDisplay);
   return file.getUrl();
@@ -135,8 +135,8 @@ function helpSite() {
 
 
 /*
- * The following is taken from the Google Apps Script example for 
- * [reading docs](http://goo.gl/TigQZ). It's modified to build a 
+ * The following is taken from the Google Apps Script example for
+ * [reading docs](http://goo.gl/TigQZ). It's modified to build a
  * [GeoJSON](http://geojson.org/) object.
  *
 */
@@ -148,11 +148,11 @@ function helpSite() {
 //   - sheet: the sheet object that contains the data to be processed
 //   - range: the exact range of cells where the data is stored
 //   - columnHeadersRowIndex: specifies the row number where the column names are stored.
-//       This argument is optional and it defaults to the row immediately above range; 
+//       This argument is optional and it defaults to the row immediately above range;
 // Returns an Array of objects.
 function getRowsData(sheet, range, columnHeadersRowIndex) {
   if (range.getRowIndex() == 1) {
-    range = range.offset(1,0);
+    range = range.offset(1, 0);
   }
   columnHeadersRowIndex = columnHeadersRowIndex || range.getRowIndex() - 1;
   var numColumns = range.getEndColumn() - range.getColumn() + 1;
@@ -174,27 +174,27 @@ function getObjects(data, keys) {
     headersRaw.indexOf(settings.long),
     headersRaw.indexOf(settings.lat)
    ];
-  
+
   // For each row
   for (var i = 0; i < data.length; ++i) {
     // If we have an id, long, and lat
     if (data[i][settingsIndex[0]] && data[i][settingsIndex[1]] && data[i][settingsIndex[2]]) {
       // Define a new GeoJSON feature object
       var feature = {
-        "type": "Feature",
+        'type': 'Feature',
         // Get ID from UI
-        "id": data[i][settingsIndex[0]],
-        "geometry": {
-          "type": 'Point',
+        'id': data[i][settingsIndex[0]],
+        'geometry': {
+          'type': 'Point',
           // Get coordinates from UIr
-          "coordinates": [data[i][settingsIndex[1]], data[i][settingsIndex[2]]]
+          'coordinates': [data[i][settingsIndex[1]], data[i][settingsIndex[2]]]
         },
         // Place holder for properties object
-        "properties": {}
+        'properties': {}
       };
-      
+
       var hasData = false;
-      
+
       // for each feild in row [i]
       for (var j = 0; j < data[i].length; ++j) {
         var cellData = data[i][j];
@@ -238,11 +238,11 @@ function normalizeHeaders(headers) {
 //   "Market Cap (millions) -> "marketCapMillions
 //   "1 number at the beginning is ignored" -> "numberAtTheBeginningIsIgnored"
 function normalizeHeader(header) {
-  var key = "";
+  var key = '';
   var upperCase = false;
   for (var i = 0; i < header.length; ++i) {
     var letter = header[i];
-    if (letter == " " && key.length > 0) {
+    if (letter == ' ' && key.length > 0) {
       upperCase = true;
       continue;
     }
@@ -266,7 +266,7 @@ function normalizeHeader(header) {
 // Arguments:
 //   - cellData: string
 function isCellEmpty(cellData) {
-  return typeof(cellData) == "string" && cellData == "";
+  return typeof(cellData) == 'string' && cellData == '';
 }
 
 // Returns true if the character char is alphabetical, false otherwise.
@@ -316,15 +316,15 @@ function runExercise() {
   var sheet = ss.getSheets()[1];
 
   // Get the range of cells that store employee data.
-  var activeRange = sheet.getRange("B1:F5");
+  var activeRange = sheet.getRange('B1:F5');
 
   // For every row of employee data, generate an employee object.
   var features = getColumnsData(sheet, activeRange);
 
   var thirdEmployee = features[2];
-  var stringToDisplay = "The third column is: " + thirdEmployee.firstName + " " + thirdEmployee.lastName;
-  stringToDisplay += " (id #" + thirdEmployee.employeeId + ") working in the ";
-  stringToDisplay += thirdEmployee.department + " department and with phone number ";
+  var stringToDisplay = 'The third column is: ' + thirdEmployee.firstName + ' ' + thirdEmployee.lastName;
+  stringToDisplay += ' (id #' + thirdEmployee.employeeId + ') working in the ';
+  stringToDisplay += thirdEmployee.department + ' department and with phone number ';
   stringToDisplay += thirdEmployee.phoneNumber;
   ss.msgBox(stringToDisplay);
 }
@@ -359,7 +359,7 @@ function arrayTranspose(data) {
 //   - sheet: the sheet object that contains the data to be processed
 //   - range: the exact range of cells where the data is stored
 //   - rowHeadersColumnIndex: specifies the column number where the row names are stored.
-//       This argument is optional and it defaults to the column immediately left of the range; 
+//       This argument is optional and it defaults to the column immediately left of the range;
 // Returns an Array of objects.
 function getColumnsData(sheet, range, rowHeadersColumnIndex) {
   rowHeadersColumnIndex = rowHeadersColumnIndex || range.getColumnIndex() - 1;
