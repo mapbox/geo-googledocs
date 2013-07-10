9befd87eb1d24266190b89ed0d9337a672d7c02b
Jeremy Heffner <jerheff@gmail.com>
2013-07-10 18:02:22 -0400

    Fix passing of key to getApiResponse

--- MapBox.js
+++ MapBox.js
@@ -387,7 +387,7 @@ function closeUiGc() {
 // Send address to api
 function getApiResponse(address, api, key) {
   var geocoder = geocoders[api],
-      url = geocoder.query(encodeURI((address), key));
+      url = geocoder.query(encodeURI(address), encodeURI(key));
   
   // If the geocoder returns a response, parse it and return components
   // If the geocoder responds poorly or doesn't response, try again
